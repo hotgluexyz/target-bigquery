a038271cca18640f17327b9738a6b19d0bb61c8a
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-12-04 11:09:51 -0300

    feat: use new table name sanitization method

--- target_bigquery/processhandler.py
+++ target_bigquery/processhandler.py
@@ -15,7 +15,8 @@ from google.cloud.exceptions import NotFound
 from jsonschema.validators import validator_for
 
 from target_bigquery.encoders import DecimalEncoder
-from target_bigquery.schema import build_schema, cleanup_record, create_valid_bigquery_name, format_record_to_schema
+from target_bigquery.schema import build_schema, cleanup_record, create_valid_bigquery_name, \
+    format_record_to_schema, create_valid_bigquery_table_name
 
 from target_bigquery.simplify_json_schema import simplify
 from target_bigquery.validate_json_schema import validate_json_schema_completeness, \
@@ -86,9 +87,16 @@ class BaseProcessHandler(object):
                 self.table_prefix, msg.stream, self.table_suffix
             )
         
-        self.tables[msg.stream] = create_valid_bigquery_name(
-            self.tables[msg.stream]
-        ) if self.force_alphanumeric_table_names else self.tables[msg.stream]
+        # first perform the safe sanitization, only replace the invalid characters
+        # that would make the target fail
+        self.tables[msg.stream] = create_valid_bigquery_table_name(self.tables[msg.stream])
+
+        # if `force_alphanumeric_table_names` perform the more strict sanitization
+        # we kept this to keep the backward compatibility with the previous behavior
+        if self.force_alphanumeric_table_names:
+            self.tables[msg.stream] = create_valid_bigquery_name(
+                self.tables[msg.stream]
+            )
 
         self.schemas[msg.stream] = msg.schema
         validator_cls = validator_for(msg.schema)
